9b8e9af8edaa53482c1ea81699d66a0aeb578a2c
Brendon Kuleck <brendonkuleck@gmail.com>
2018-01-15 23:21:22 -0800

    Refactor into Functional Components

---
+++ src/components/search_bar.js
@@ -0,0 +1,12 @@
+// Generate no-frills HTML input
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return <input />;
+  };
+};
+
+// new SearchBar (instance ex)
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyDCgD1fx18NbRaSGFHnVJy63ryMhezFO34';
 
 // create a new component, which should produce some html
 
 //COMPONENT class
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
 };
 
 //COMPONENT INSTANCE
